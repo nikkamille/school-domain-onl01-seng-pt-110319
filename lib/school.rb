0aaac36ff0a29ae15eadd55f63afaf21d5d3bb13
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-26 07:55:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,7 @@ class School
   attr_accessor :name, :roster
   
   def add_student(student_name, grade)
-    roster[grade] = []
+    roster[grade] ||= []
     roster[grade] << student_name
   end
   
