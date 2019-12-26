4f17c369a336caee36cd737e3f52b07605f78d52
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-26 07:36:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,5 @@
 class School
-  def initialize(name)
-    @name = name
-  end
-  
-  attr_accessor :name
-  
-  def roster(student_name)
-    @roster = roster
+  def initialize(roster)
     roster = {}
   end
 end
